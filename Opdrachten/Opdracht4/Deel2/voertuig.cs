7f7f214b1ac63835f64c67c3f2e46d3f756c93db
MorgBeka <morgbeka@student.arteveldehs.be>
2018-11-14 11:57:09 +0100

    opdracht4

---
+++ Opdrachten/Opdracht4/Deel2/voertuig.cs
@@ -0,0 +1,55 @@
+class Voertuig
+{
+    //velden
+    //properties
+    //methods
+    //constructors
+    //...
+
+    private int laadvermogen;
+    //private bool motor;
+    // private string brandstof;
+    //merk
+
+//properties
+
+    // int Laadvermogen
+    // {
+    //   get;
+    //   set;
+    // }
+
+    public int Laadvermogen
+    { 
+        get 
+       {
+        return laadvermogen;
+       } 
+       set 
+       {
+           laadvermogen=value;
+       }
+        
+    }
+
+    public string Motor { get; set;}
+    public string Kleur { get; set;}
+    
+    //Constructor 
+    public Voertuig(int laadvermogen, string kleur)
+    {
+        // Laadvermogen = laadvermogen;
+        this.laadvermogen = laadvermogen;
+        Kleur = Kleur;
+    }
+
+    public Voertuig()
+    {
+
+//uitlezen
+    }
+    public string ToString()
+    {
+        return "Voertuig met laadvermogen: " + this.laadvermogen + ", met kleur: " + this.Kleur;
+    }
+}
