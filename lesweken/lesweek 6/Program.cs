434eef3e12b55c4bbb120a2235abe784166d3dc4
MorgBeka <morgbeka@student.arteveldehs.be>
2018-11-17 19:52:57 +0100

    lesweek

---
+++ lesweken/lesweek 6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lesweek_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        string[,] menu = new string[ , ] { { "Plat water", "1" }, { "cola 25 cl", "1.5" },
+                                        { "cola 33 cl", "2" }, {"witte wijn", "3"},
+                                        {"pils", "2"}, {"toast", "5"}, {"kaasplank", "4"} };
+
+        Console.WriteLine(menu[0, 0]);
+
+
+                }
+            }
+}
---
+++ lesweken/lesweek 7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lesweek7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //werkmehtode 1
+            Voertuig voertuig1 = new Voertuig();
+            voertuig1.Laadvermogen = 12;
+            voertuig1.Kleur = "blauw";
+            Console.WriteLine(voertuig1.ToString());
+
+            //werkmethode 2
+            Voertuig voertuig2 = new Voertuig(30, "rood");
+            Console.WriteLine(voertuig2.ToString());
+            // auto1.laadvermogen = 12;
+
+            Auto auto1 = new Auto();
+            Console.WriteLine(auto1.ToString());
+          
+        }
+    }
+
+    // internal class Voertuig
+    // {
+    //      private string laadvermogen;
+    //}
+    
+}
+
+
