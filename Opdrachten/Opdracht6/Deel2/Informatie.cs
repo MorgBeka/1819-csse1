1a215f3542edb5462b149208bc1940ba8abae9b7
MorgBeka <morgbeka@student.arteveldehs.be>
2018-12-09 19:41:21 +0100

    Opdracht 6 deel 2

---
+++ Opdrachten/Opdracht6/Deel2/Informatie.cs
@@ -0,0 +1,16 @@
+namespace deel2
+{
+    public interface Informatie
+    {
+        //private string type;
+        //public
+        /*string GroepType
+        {
+            get;
+            set;
+        } */
+
+        string ToString();
+        
+    }
+}
---
+++ Opdrachten/Opdracht6/Deel2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Opdracht5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
